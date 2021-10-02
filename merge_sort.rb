d01aade6a507d2d9765c505000c502a48c2b39b1
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-02 17:43:13 -0500

    Merge sort implementation

---
+++ merge_sort.rb
@@ -0,0 +1,31 @@
+class MergeSort
+  # @param [Object] list
+  def sort(list)
+    if list.length <= 1
+      return list
+    end
+
+    half = list.length / 2
+
+    left = sort list[0...half]
+    right = sort list[half...]
+    merge(left, right)
+  end
+
+  private
+
+  # @param [Object] left
+  # @param [Object] right
+  def merge(left, right)
+    merged_list = []
+
+    while !left.empty? && !right.empty?
+      merged_list.push(left[0] < right[0] ? left.shift : right.shift)
+    end
+
+    # Merge remaining
+    left.each { |i| merged_list.push i }
+    right.each { |i| merged_list.push i }
+    merged_list
+  end
+end
